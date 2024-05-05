c15f97bd3132a26472faea4c89b62fedbc35fb17
JangWoojun <woojun0107@naver.com>
2024-05-05 18:25:16 +0900

    feat: SarangbangFragment API 데이터 연결

--- app/src/main/java/com/woojun/nado/fragment/SarangbangFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/SarangbangFragment.kt
@@ -9,15 +9,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
-import com.woojun.nado.Lecture
 import com.woojun.nado.R
 import com.woojun.nado.ToolTip
 import com.woojun.nado.UpdateAdapter
 import com.woojun.nado.Utils.dpToPx
 import com.woojun.nado.ViewPagerAdapter
+import com.woojun.nado.database.ViewModel
 import com.woojun.nado.databinding.FragmentSarangbangBinding
 import kotlin.math.ceil
 
@@ -28,12 +29,9 @@ class SarangbangFragment : Fragment() {
 
     private val viewPagerHandler = Handler()
     private var handlerRunnable = Runnable { binding.viewPager.currentItem = binding.viewPager.currentItem + 1 }
-    private val list = mutableListOf(
-        Lecture("1", "", "", "https://www.naver.com/"),
-        Lecture("2", "", "", "https://www.youtube.com/"),
-        Lecture("3", "", "", "https://github.com/"),
-        Lecture("4", "", "", "https://www.acmicpc.net/"),
-    )
+
+    private lateinit var viewModel: ViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +48,12 @@ class SarangbangFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity())[ViewModel::class.java]
+        viewModel.getOnlineCourseList().observe(viewLifecycleOwner) { apiData ->
+            binding.lectureList.adapter = UpdateAdapter(apiData)
+            binding.lectureList.layoutManager = LinearLayoutManager(requireContext())
+        }
+
         binding.infoButton.setOnClickListener {
             val balloon = ToolTip.createBalloon(
                 requireContext(),
@@ -60,44 +64,60 @@ class SarangbangFragment : Fragment() {
             )
             balloon.showAlignBottom(it)
         }
+        viewModel.getOnlineCourseList().observe(viewLifecycleOwner) { apiData ->
+            val list = apiData.subList(0, 4)
+            binding.viewPager.apply {
+                this.adapter = ViewPagerAdapter(list)
+                this.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+                offscreenPageLimit = 3
 
-        binding.viewPager.apply {
-            this.adapter = ViewPagerAdapter(list)
-            this.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
-            offscreenPageLimit = 3
+                val pageMargin = requireContext().dpToPx(20f)
+                val offset = requireContext().dpToPx(20f)
 
-            val pageMargin = requireContext().dpToPx(20f)
-            val offset = requireContext().dpToPx(20f)
-
-            setPageTransformer { page, position ->
-                val pageOffset = position * -(2 * offset + pageMargin)
-                if (ViewCompat.getLayoutDirection(binding.viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
-                    page.translationX = -pageOffset
-                } else {
-                    page.translationX = pageOffset
+                setPageTransformer { page, position ->
+                    val pageOffset = position * -(2 * offset + pageMargin)
+                    if (ViewCompat.getLayoutDirection(binding.viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                        page.translationX = -pageOffset
+                    } else {
+                        page.translationX = pageOffset
+                    }
                 }
+
+                registerOnPageChangeCallback(object : OnPageChangeCallback() {
+                    override fun onPageSelected(position: Int) {
+                        super.onPageSelected(position)
+                        viewPagerHandler.removeCallbacks(handlerRunnable)
+                        viewPagerHandler.postDelayed(handlerRunnable, 2000)
+
+                        binding.indicator.selectDot(position % list.size)
+                    }
+                })
+
+                setCurrentItem(Int.MAX_VALUE / 2 - ceil(list.size.toDouble() / 2).toInt() - 1, false)
+
             }
+            binding.indicator.createDotPanel(list.size, R.drawable.indicator_dot_off, R.drawable.indicator_dot_on, 0)
 
-            registerOnPageChangeCallback(object : OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                    viewPagerHandler.removeCallbacks(handlerRunnable)
-                    viewPagerHandler.postDelayed(handlerRunnable, 2000)
-
-                    binding.indicator.selectDot(position % list.size)
-                }
-            })
-
-            setCurrentItem(Int.MAX_VALUE / 2 - ceil(list.size.toDouble() / 2).toInt() - 1, false)
-
-
+            binding.lectureList.adapter = LectureAdapter(list)
+            binding.lectureList.layoutManager = LinearLayoutManager(requireContext())
         }
-        binding.indicator.createDotPanel(list.size, R.drawable.indicator_dot_off, R.drawable.indicator_dot_on, 0)
-
-        binding.lectureList.adapter = LectureAdapter(list)
-        binding.lectureList.layoutManager = LinearLayoutManager(requireContext())
 
 
+        binding.orderLatestButton.setOnClickListener {
+            resetText()
+            it as TextView
+            it.setTextColor(Color.parseColor("#FF5656"))
+        }
+        binding.orderPopularityButton.setOnClickListener {
+            resetText()
+            it as TextView
+            it.setTextColor(Color.parseColor("#FF5656"))
+        }
+        binding.orderNameButton.setOnClickListener {
+            resetText()
+            it as TextView
+            it.setTextColor(Color.parseColor("#FF5656"))
+        }
     }
 
     private fun resetText() {
