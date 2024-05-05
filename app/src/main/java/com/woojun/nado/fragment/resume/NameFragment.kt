960f798f6a3c5f7893b4f5e8d1091d257f69fe1c
JangWoojun <woojun0107@naver.com>
2024-05-05 15:06:54 +0900

    feat: NameFragment 기능 추가
    STT 중단 및 sendButton 기능 추가, Toast 메시지 텍스트 변경, id 변경

--- app/src/main/java/com/woojun/nado/fragment/resume/NameFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/resume/NameFragment.kt
@@ -1,30 +1,37 @@
 package com.woojun.nado.fragment.resume
 
 import android.Manifest
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.woojun.nado.R
 import com.woojun.nado.databinding.FragmentNameBinding
 import java.util.Locale
 
+
 class NameFragment : Fragment() {
 
     private var _binding: FragmentNameBinding? = null
     private val binding get() = _binding!!
+
     private lateinit var speechRecognizer: SpeechRecognizer
     private lateinit var recognizerIntent: Intent
+
+    private var isStart = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,13 +53,22 @@ class NameFragment : Fragment() {
         }
 
         binding.button2.setOnClickListener {
-            if (binding.input.text.isNotEmpty()) {
+            if (binding.nameInput.text.isNotEmpty()) {
                 findNavController().navigate(
                     R.id.resumeWriteFragment, Bundle().apply {
-                        this.putString("name", binding.input.text.toString())
+                        this.putString("name", binding.nameInput.text.toString())
                     }
                 )
+            } else {
+                Toast.makeText(requireContext(), "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        binding.sendButton.setOnClickListener {
+            val inputMethodManager = binding.nameInput.context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+            binding.button1.isFocusable = true
         }
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireContext())
@@ -61,12 +77,19 @@ class NameFragment : Fragment() {
         recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "작성하실 단어를 말하세요!")
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "작성하실 단어를 말하세요.")
 
         binding.micButton.setOnClickListener {
             requestPermission()
-            Toast.makeText(requireContext(), "작성하실 단어를 말하세요!", Toast.LENGTH_LONG).show()
-            speechRecognizer.startListening(recognizerIntent)
+            if (isStart) {
+                speechRecognizer.stopListening()
+                speechRecognizer.destroy()
+                Toast.makeText(requireContext(), "중지하셨습니다.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "작성하실 단어를 말하세요.", Toast.LENGTH_LONG).show()
+                speechRecognizer.startListening(recognizerIntent)
+            }
+            isStart = !isStart
         }
     }
 
@@ -112,7 +135,7 @@ class NameFragment : Fragment() {
             val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
             if (!matches.isNullOrEmpty()) {
                 val result = matches[0]
-                binding.input.setText("${binding.input.text} $result")
+                binding.nameInput.setText("${binding.nameInput.text} $result")
             }
         }
 
