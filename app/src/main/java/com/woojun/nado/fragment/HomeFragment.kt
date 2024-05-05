c2a8abd1325021bbd54fa08b9b11c381602e684a
JangWoojun <woojun0107@naver.com>
2024-05-05 17:59:20 +0900

    feat: HomeFragment API 데이터 연결

--- app/src/main/java/com/woojun/nado/fragment/HomeFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/HomeFragment.kt
@@ -6,19 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.woojun.nado.Lecture
-import com.woojun.nado.MainActivity
+import com.bumptech.glide.Glide
 import com.woojun.nado.R
 import com.woojun.nado.ToolTip.createBalloon
 import com.woojun.nado.UpdateAdapter
 import com.woojun.nado.WebViewActivity
+import com.woojun.nado.database.ViewModel
 import com.woojun.nado.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -33,6 +36,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         binding.infoButton.setOnClickListener {
             val balloon = createBalloon(
@@ -57,27 +61,44 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.setting)
         }
 
-        binding.lectureBox1.setOnClickListener {
-            val lecture = Lecture("", "", "", "")
-            requireActivity().startActivity(Intent(
-                requireActivity(), WebViewActivity::class.java).apply {
-                    this.putExtra("url", lecture.url)
-                }
-            )
+
+
+        viewModel = ViewModelProvider(requireActivity())[ViewModel::class.java]
+        viewModel.getOnlineCourseList().observe(viewLifecycleOwner) { apiData ->
+            binding.updateList.adapter = UpdateAdapter(apiData)
+            binding.updateList.layoutManager = LinearLayoutManager(requireContext())
         }
+        viewModel.getRecommendationOnlineCourseList().observe(viewLifecycleOwner) { apiData ->
+            Glide.with(binding.root.context)
+                .load(apiData[0].image)
+                .centerCrop()
+                .into(binding.lectureBox1Image)
+            binding.lectureBox1MainText.text = apiData[0].titleText
+            binding.lectureBox1SubText.text = apiData[0].subText
 
-        binding.lectureBox2.setOnClickListener {
-            val lecture = Lecture("", "", "", "")
-            requireActivity().startActivity(Intent(
-                requireActivity(), WebViewActivity::class.java).apply {
-                this.putExtra("url", lecture.url)
-                }
-            )
+            Glide.with(binding.root.context)
+                .load(apiData[1].image)
+                .centerCrop()
+                .into(binding.lectureBox2Image)
+            binding.lectureBox2MainText.text = apiData[1].titleText
+            binding.lectureBox2SubText.text = apiData[1].subText
+
+            binding.lectureBox1.setOnClickListener {
+                requireActivity().startActivity(Intent(
+                    requireActivity(), WebViewActivity::class.java).apply {
+                        this.putExtra("url", apiData[0].url)
+                    }
+                )
+            }
+
+            binding.lectureBox2.setOnClickListener {
+                requireActivity().startActivity(Intent(
+                    requireActivity(), WebViewActivity::class.java).apply {
+                        this.putExtra("url", apiData[1].url)
+                    }
+                )
+            }
         }
-
-
-        binding.updateList.adapter = UpdateAdapter(mutableListOf())
-        binding.updateList.layoutManager = LinearLayoutManager(requireContext())
 
     }
 
