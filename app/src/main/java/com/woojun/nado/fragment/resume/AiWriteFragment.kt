693d4117bb570d6b2aacd4e206693dfd8551f65a
JangWoojun <woojun0107@naver.com>
2024-05-05 13:17:57 +0900

    feat: Resume 관련  Fragment들 추가

---
+++ app/src/main/java/com/woojun/nado/fragment/resume/AiWriteFragment.kt
@@ -0,0 +1,36 @@
+package com.woojun.nado.fragment.resume
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.nado.R
+import com.woojun.nado.databinding.FragmentAiWriteBinding
+import com.woojun.nado.databinding.FragmentSpellingBinding
+
+class AiWriteFragment : Fragment() {
+    private var _binding: FragmentAiWriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAiWriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/woojun/nado/fragment/resume/ResumeFragment.kt
@@ -0,0 +1,36 @@
+package com.woojun.nado.fragment.resume
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.nado.R
+import com.woojun.nado.databinding.FragmentResumeBinding
+import com.woojun.nado.databinding.FragmentSettingBinding
+
+class ResumeFragment : Fragment() {
+    private var _binding: FragmentResumeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResumeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
