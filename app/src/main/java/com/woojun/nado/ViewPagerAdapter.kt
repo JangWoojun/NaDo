74c1c9548e6abb871884179de2e8778defc6a47d
JangWoojun <woojun0107@naver.com>
2024-05-05 10:49:57 +0900

    feat: ViewPagerAdapter 및 Indicator 연결 및 구현 완료

---
+++ app/src/main/java/com/woojun/nado/ViewPagerAdapter.kt
@@ -0,0 +1,55 @@
+package com.woojun.nado
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.updateLayoutParams
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.woojun.nado.Utils.dpToPx
+import com.woojun.nado.databinding.LeectureItemBinding
+
+
+class ViewPagerAdapter(private val lectureList: MutableList<Lecture>) : RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder>()  {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
+        val binding = LeectureItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewPagerHolder(binding).also { handler ->
+            binding.box.setOnClickListener {
+                parent.context.startActivity(
+                    Intent(parent.context, WebViewActivity::class.java)
+                        .putExtra("url", lectureList[handler.position % lectureList.size].url)
+                )
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        holder.bind(lectureList[position % lectureList.size])
+    }
+
+
+    override fun getItemCount(): Int = Int.MAX_VALUE
+
+
+    class ViewPagerHolder(private val binding: LeectureItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(lecture: Lecture) {
+            binding.root.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                marginStart = binding.root.context.dpToPx(40f).toInt()
+                marginEnd = binding.root.context.dpToPx(40f).toInt()
+            }
+
+            if (binding.root.context != null) {
+                Glide.with(binding.root.context)
+                    .load(lecture.image)
+                    .centerCrop()
+                    .into(binding.image)
+            }
+
+            binding.mainText.text = lecture.titleText
+        }
+    }
+
+}
--- app/src/main/java/com/woojun/nado/fragment/SarangbangFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/SarangbangFragment.kt
@@ -1,16 +1,36 @@
 package com.woojun.nado.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.woojun.nado.Lecture
+import com.woojun.nado.R
 import com.woojun.nado.ToolTip
+import com.woojun.nado.Utils.dpToPx
+import com.woojun.nado.ViewPagerAdapter
 import com.woojun.nado.databinding.FragmentSarangbangBinding
+import kotlin.math.ceil
+
 
 class SarangbangFragment : Fragment() {
     private var _binding: FragmentSarangbangBinding? = null
     private val binding get() = _binding!!
+
+    private val viewPagerHandler = Handler()
+    private var handlerRunnable = Runnable { binding.viewPager.currentItem = binding.viewPager.currentItem + 1 }
+    private val list = mutableListOf(
+        Lecture("1", "", "", "https://www.naver.com/"),
+        Lecture("2", "", "", "https://www.youtube.com/"),
+        Lecture("3", "", "", "https://github.com/"),
+        Lecture("4", "", "", "https://www.acmicpc.net/"),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -37,7 +57,50 @@ class SarangbangFragment : Fragment() {
             balloon.showAlignBottom(it)
         }
 
+        val adapter = ViewPagerAdapter(list)
 
+        binding.viewPager.apply {
+            this.adapter = adapter
+            this.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+            offscreenPageLimit = 3
+
+            val pageMargin = requireContext().dpToPx(20f)
+            val offset = requireContext().dpToPx(20f)
+
+            setPageTransformer { page, position ->
+                val pageOffset = position * -(2 * offset + pageMargin)
+                if (ViewCompat.getLayoutDirection(binding.viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                    page.translationX = -pageOffset
+                } else {
+                    page.translationX = pageOffset
+                }
+            }
+
+            registerOnPageChangeCallback(object : OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    viewPagerHandler.removeCallbacks(handlerRunnable)
+                    viewPagerHandler.postDelayed(handlerRunnable, 2000)
+
+                    binding.indicator.selectDot(position % list.size)
+                }
+            })
+
+            setCurrentItem(Int.MAX_VALUE / 2 - ceil(list.size.toDouble() / 2).toInt() - 1, false)
+
+
+        }
+        binding.indicator.createDotPanel(list.size, R.drawable.indicator_dot_off, R.drawable.indicator_dot_on, 0)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewPagerHandler.removeCallbacks(handlerRunnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewPagerHandler.postDelayed(handlerRunnable, 2000)
     }
 
     override fun onDestroyView() {
