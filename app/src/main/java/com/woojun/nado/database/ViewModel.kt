a45016ef7fb61fad45caafabfff99790ac59065e
JangWoojun <woojun0107@naver.com>
2024-05-06 21:12:07 +0900

    feat: getEducationList 데이터 저장 및 연결

--- app/src/main/java/com/woojun/nado/database/ViewModel.kt
+++ app/src/main/java/com/woojun/nado/database/ViewModel.kt
@@ -1,10 +1,15 @@
 package com.woojun.nado.database
 
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.woojun.nado.adapter.EducationAdapter
+import com.woojun.nado.data.Education
 import com.woojun.nado.data.Lecture
 import com.woojun.nado.data.OnlineCourseList
+import com.woojun.nado.data.TbViewProgram
 import com.woojun.nado.network.RetrofitAPI
 import com.woojun.nado.network.RetrofitClient
 import retrofit2.Call
@@ -17,6 +22,10 @@ class ViewModel : ViewModel() {
 
     private val _recommendationOnlineCourse = MutableLiveData<MutableList<Lecture>>()
     private val recommendationOnlineCourse: LiveData<MutableList<Lecture>> = _recommendationOnlineCourse
+
+    private val _educationList = MutableLiveData<MutableList<Education>>()
+    private val educationList: LiveData<MutableList<Education>> = _educationList
+
     fun loadOnlineCourse(callback: () -> Unit) {
         val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
         val call: Call<OnlineCourseList> = retrofitAPI.getOnlineCourseList()
@@ -69,11 +78,37 @@ class ViewModel : ViewModel() {
         })
     }
 
+    fun loadEducationList(callback: () -> Unit) {
+        val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
+        val call: Call<TbViewProgram> = retrofitAPI.getEducationList()
+
+        call.enqueue(object : Callback<TbViewProgram> {
+            override fun onResponse(
+                call: Call<TbViewProgram>,
+                response: Response<TbViewProgram>
+            ) {
+                if (response.isSuccessful) {
+                    _educationList.value = response.body()?.map {
+                        Education(it.SUBJECT, "${it.APPLICATIONSTARTDATE} ~ ${it.APPLICATIONENDDATE}", it.APPLY_STATE, it.VIEWDETAIL)
+                    }?.toMutableList()
+                }
+            }
+
+            override fun onFailure(call: Call<TbViewProgram>, t: Throwable) {
+                callback()
+            }
+        })
+    }
+
     fun getOnlineCourseList(): LiveData<MutableList<Lecture>> {
         return onlineCourse
     }
 
     fun getRecommendationOnlineCourseList(): LiveData<MutableList<Lecture>> {
         return recommendationOnlineCourse
+    }
+
+    fun getEducationList(): LiveData<MutableList<Education>> {
+        return educationList
     }
 }
--- app/src/main/java/com/woojun/nado/fragment/EducationFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/EducationFragment.kt
@@ -1,16 +1,29 @@
 package com.woojun.nado.fragment
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.woojun.nado.R
+import com.woojun.nado.adapter.EducationAdapter
+import com.woojun.nado.database.ViewModel
 import com.woojun.nado.databinding.FragmentEducationBinding
 
 class EducationFragment : Fragment() {
     private var _binding: FragmentEducationBinding? = null
 
     private val binding get() = _binding!!
+    private lateinit var viewModel: ViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +40,36 @@ class EducationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tooltipButton.setOnClickListener {
+            val customDialog = Dialog(requireContext())
+            customDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            customDialog.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+            customDialog.setContentView(R.layout.tooltip_layout)
+
+            customDialog.findViewById<TextView>(R.id.text_view).text = """
+                이 화면은 서울시 어르신 취업지원 센터 교육정보에 대한 안내를 드리는 곳입니다.
+
+                관심있는 교육을 선택하면, 자동으로 <서울시어르신취업센터>의 해당 안내문으로 연결됩니다. 
+
+                이전으로 돌아가기: 다시 <학습하기>의 메인 화면으로 이동합니다.
+            """.trimIndent()
+
+            customDialog.show()
+        }
+
+        viewModel = ViewModelProvider(requireActivity())[ViewModel::class.java]
+        viewModel.getEducationList().observe(viewLifecycleOwner) { list ->
+            Log.d("확인", list.toString())
+            binding.educationList.adapter = EducationAdapter(list)
+            binding.educationList.layoutManager = LinearLayoutManager(requireContext())
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 
 }
