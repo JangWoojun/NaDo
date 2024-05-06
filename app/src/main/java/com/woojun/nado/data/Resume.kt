11efd6657ef1bec2583df71f6ee2da9aef411934
JangWoojun <woojun0107@naver.com>
2024-05-06 18:29:36 +0900

    feat: Resume 데이터 클래스 추가

---
+++ app/src/main/java/com/woojun/nado/data/Resume.kt
@@ -0,0 +1,12 @@
+package com.woojun.nado.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Resume(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val content: String
+)
