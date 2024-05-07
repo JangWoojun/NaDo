854e06b236f0b3d58a6391a127e2e98cc1a92259
JangWoojun <woojun0107@naver.com>
2024-05-08 07:35:41 +0900

    feat: Interview 작성 로직 추가

--- app/src/main/java/com/woojun/nado/fragment/interview/InterviewWriteFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/interview/InterviewWriteFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.woojun.nado.R
-import com.woojun.nado.databinding.FragmentInterviewBinding
+import com.woojun.nado.data.Interview
+import com.woojun.nado.database.AppDatabase
 import com.woojun.nado.databinding.FragmentInterviewWriteBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class InterviewWriteFragment : Fragment() {
 
@@ -28,7 +34,22 @@ class InterviewWriteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val question = arguments?.getString("question")
 
+        binding.titleText.text = "Q. $question"
+        binding.button1.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.button2.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val interviewDao = AppDatabase.getDatabase(requireContext())?.interviewDao()
+                interviewDao?.insertInterview(Interview(question = question ?: "", content = binding.contentInput.text.toString()))
+
+                withContext(Dispatchers.Main) {
+                    findNavController().navigate(R.id.interviewListFragment)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
