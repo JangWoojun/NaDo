25c0954118b0148702f2e6930a105954cc8aa88b
JangWoojun <woojun0107@naver.com>
2024-05-08 12:03:01 +0900

    feat: AiInterviewAdapter 연결 및 삭제 로직 구현

--- app/src/main/java/com/woojun/nado/fragment/interview/MyInterviewFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/interview/MyInterviewFragment.kt
@@ -1,11 +1,25 @@
 package com.woojun.nado.fragment.interview
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.TypefaceSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.woojun.nado.R
+import com.woojun.nado.adapter.AiInterviewAdapter
+import com.woojun.nado.database.AppDatabase
+import com.woojun.nado.database.Preferences
 import com.woojun.nado.databinding.FragmentMyInterviewBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MyInterviewFragment : Fragment() {
 
@@ -26,7 +40,36 @@ class MyInterviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            val aiInterviewDao = AppDatabase.getDatabase(requireContext())?.aiInterviewDao()
+            val adapter = AiInterviewAdapter(aiInterviewDao!!.getAiInterviewList())
 
+            withContext(Dispatchers.Main) {
+
+                binding.aiInterviewList.adapter = adapter
+                binding.aiInterviewList.layoutManager = LinearLayoutManager(requireContext())
+
+                binding.button1.setOnClickListener {
+                    findNavController().popBackStack()
+                }
+                binding.button2.setOnClickListener {
+                    if (adapter.getIndex() != null) {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val aiInterview = aiInterviewDao!!.getAiInterviewList()[adapter.getIndex()!!]
+                            aiInterviewDao.deleteAiInterview(aiInterview)
+
+                            withContext(Dispatchers.Main) {
+                                adapter.removeItem(adapter.getIndex()!!)
+                            }
+                        }
+                    } else {
+                        Toast.makeText(requireContext(), "인터뷰를 선택해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+
+        }
     }
 
     override fun onDestroyView() {
