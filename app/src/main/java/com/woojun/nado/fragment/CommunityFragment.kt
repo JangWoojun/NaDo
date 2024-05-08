438e82451b37e272fecc9175e59932147cea95aa
JangWoojun <woojun0107@naver.com>
2024-05-08 13:52:11 +0900

    feat: CommunityFragment 카테고리 이동 로직 구현

--- app/src/main/java/com/woojun/nado/fragment/CommunityFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/CommunityFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.woojun.nado.R
 import com.woojun.nado.util.ToolTip
 import com.woojun.nado.databinding.FragmentCommunityBinding
 
@@ -35,6 +37,18 @@ class CommunityFragment : Fragment() {
                         "주제별 게시판을 둘러보세요."
             )
             balloon.showAlignBottom(it)
+        }
+
+        val buttonList = listOf(binding.button1, binding.button2, binding.button3, binding.button4, binding.button5, binding.button6)
+        buttonList.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                findNavController().navigate(
+                    R.id.communityListFragment,
+                    Bundle().apply {
+                        this.putInt("category", index)
+                    }
+                )
+            }
         }
     }
 
