7a2553254bca38511eec5c6d29b36a17ce9abcc7
JangWoojun <woojun0107@naver.com>
2024-05-11 03:41:13 +0900

    feat: STT 로직 추가

--- app/src/main/java/com/woojun/nado/fragment/community/CommunityListFragment.kt
+++ app/src/main/java/com/woojun/nado/fragment/community/CommunityListFragment.kt
@@ -1,7 +1,13 @@
 package com.woojun.nado.fragment.community
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.text.Editable
 import android.text.SpannableString
 import android.text.TextWatcher
@@ -13,6 +19,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.woojun.nado.R
@@ -29,6 +37,7 @@ import com.woojun.nado.util.ToolTip
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.Locale
 
 
 class CommunityListFragment : Fragment() {
@@ -42,6 +51,11 @@ class CommunityListFragment : Fragment() {
     private lateinit var originalList: MutableList<BoardListItem>
 
     private lateinit var adapter: CommunityAdapter
+
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private lateinit var recognizerIntent: Intent
+
+    private var isStart = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -180,11 +194,83 @@ class CommunityListFragment : Fragment() {
             }
         })
 
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireContext())
+        speechRecognizer.setRecognitionListener(recognitionListener)
+
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "작성하실 단어를 말하세요.")
+
+        binding.micButton.setOnClickListener {
+            requestPermission()
+            if (isStart) {
+                speechRecognizer.stopListening()
+                speechRecognizer.destroy()
+                Toast.makeText(requireContext(), "중지하셨습니다.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "작성하실 단어를 말하세요.", Toast.LENGTH_LONG).show()
+                speechRecognizer.startListening(recognizerIntent)
+            }
+            isStart = !isStart
+        }
+    }
+
+    private fun requestPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.RECORD_AUDIO), 0)
+        }
+    }
+
+    private val recognitionListener = object : RecognitionListener {
+        override fun onReadyForSpeech(params: Bundle?) {}
+
+        override fun onBeginningOfSpeech() {}
+
+        override fun onRmsChanged(rmsdB: Float) {}
+
+        override fun onBufferReceived(buffer: ByteArray?) {}
+
+        override fun onEndOfSpeech() {
+            Toast.makeText(requireContext(), "듣기 종료", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onError(error: Int) {
+            val message = when (error) {
+                SpeechRecognizer.ERROR_AUDIO -> "오디오 에러"
+                SpeechRecognizer.ERROR_CLIENT -> "클라이언트 에러"
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "퍼미션 없음"
+                SpeechRecognizer.ERROR_NETWORK -> "네트워크 에러"
+                SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "네트웍 타임아웃"
+                SpeechRecognizer.ERROR_NO_MATCH -> "찾을 수 없음"
+                SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RECOGNIZER 가 바쁨"
+                SpeechRecognizer.ERROR_SERVER -> "서버가 이상함"
+                SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "말하는 시간초과"
+                else -> "알 수 없는 오류임"
+            }
+            Toast.makeText(requireContext(), "에러 발생: $message", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onResults(results: Bundle?) {
+            val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            if (!matches.isNullOrEmpty()) {
+                val result = matches[0]
+                binding.titleInput.setText("${binding.titleInput.text} $result")
+            }
+        }
+
+        override fun onPartialResults(partialResults: Bundle?) {}
+
+        override fun onEvent(eventType: Int, params: Bundle?) {}
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        speechRecognizer.destroy()
     }
 
     private fun filterItem(title: String) {
