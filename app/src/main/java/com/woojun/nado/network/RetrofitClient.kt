1ff043e4161089ef86e33af4722865915b154a6a
JangWoojun <woojun0107@naver.com>
2024-05-11 11:23:16 +0900

    fix: AI 인터뷰 TimeOut 방지

--- app/src/main/java/com/woojun/nado/network/RetrofitClient.kt
+++ app/src/main/java/com/woojun/nado/network/RetrofitClient.kt
@@ -2,17 +2,26 @@ package com.woojun.nado.network
 
 import com.google.gson.GsonBuilder
 import com.woojun.nado.BuildConfig
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private var instance: Retrofit? = null
     private val gson = GsonBuilder().setLenient().create()
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(90, TimeUnit.SECONDS)
+        .readTimeout(90, TimeUnit.SECONDS)
+        .writeTimeout(90, TimeUnit.SECONDS)
+        .build()
+
     fun getInstance(): Retrofit {
         if(instance == null) {
             instance = Retrofit.Builder()
                 .baseUrl(BuildConfig.baseUrl)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
         }
